3e8de76e1a0993668cd24a9d18b0b51b7d8bd084
matcool <26722564+matcool@users.noreply.github.com>
2019-04-17 20:41:37 -0300

    Working <btnChoice>

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function isFunction(functionToCheck) {
     return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
 }
+function hasAttribute(attrlist, attr) {
+    return attrlist[attr] !== undefined;
+}
 
 for (let btn of document.querySelectorAll('btnToggle')) {
     btn.innerHTML = `<button type="button" class="btn btn-secondary">Off</button>`;
@@ -13,6 +16,34 @@ for (let btn of document.querySelectorAll('btnToggle')) {
         childBtn.innerHTML = btn.pressed ? 'On' : 'Off';
     };
     if (btn.attributes.on !== undefined) btn.onclick();
+}
+
+for (let btn of document.querySelectorAll('btnChoice')) {
+    let choices = [];
+    for (let child of btn.children) {
+        if (child.nodeName == 'CHOICE') {
+            choices.push(child);
+            if (hasAttribute(child.attributes, 'default')) btn.selected = child;
+        }
+    }
+    btn.innerHTML = `<div class="dropdown">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Loading
+    </button>
+    <div class="dropdown-menu"></div>
+</div>`;
+    btn.children[0].children[0].innerHTML = btn.selected.innerHTML;
+    for (let choice of choices) {
+        let option = document.createElement('a');
+        option.href = '#';
+        option.classList.add('dropdown-item');
+        option.innerHTML = choice.innerHTML;
+        option.onclick = () => {
+            btn.selected = choice;
+            btn.children[0].children[0].innerHTML = btn.selected.innerHTML;
+        };
+        btn.children[0].children[1].appendChild(option);
+    }
 }
 
 for (let btn of document.getElementsByClassName('property-button')) {
@@ -30,6 +61,8 @@ function updateProperties() {
         let key = btn.id;
         if (btn.nodeName == 'BTNTOGGLE') {
             properties[key] = btn.pressed;
+        } else if (btn.nodeName == 'BTNCHOICE') {
+            properties[key] = btn.selected.attributes.value.value;
         }
     }
     updateCode();
